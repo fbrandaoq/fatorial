6b3f7f2f0adb02ff6fbde60a2d4ef28b363f892e
fbrandaoq <fbrandaoq@gmail.com>
2024-06-05 19:45:56 -0300

    Versão 1.0

---
+++ main.py
@@ -0,0 +1,12 @@
+# funcao fatorial
+def calcular_fatorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * calcular_fatorial(n -1)
+
+# programa principal
+n = int(input('Informe um número inteiro: '))
+
+# exibe o resultado do fatorial
+print(f'Fatorial de {n}: {calcular_fatorial(n)}.')
